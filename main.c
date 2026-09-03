07699dc3514ba3badd13dac15f6f9bf8ac53230d
Aryan <aryangp291@gmail.com>
2026-09-03 14:00:23 +0000

    Learning the basics of Makefiles

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+
+
+    printf("This shall be very own Chess Engine.\nInspired by Lelouch Lamperouge from Code Geass - Lelouch of the Rebellion.\n");
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,6 @@
+hello:
+	@echo "Hello World"
+
+
+bruh:
+	@echo "Hello bruh"
